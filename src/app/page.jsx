1a8e7b1d05d7a3746408bd63c09b3c8f5ce9a82d
Pritam Das <dpritam28577@gmail.com>
2024-03-09 12:47:28 +0530

    Add ShopLayout1 component to IndexPage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,22 @@
 // import IndexPageView from "pages-sections/landing/page-view";
 import FashionTwoPageView from "pages-sections/fashion-2/page-view";
+import ShopLayout1 from "components/layouts/shop-layout-1";
 
 export const metadata = {
   title: "Bazaar - Next.js E-commerce Template",
   description: `Bazaar is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store`,
-  authors: [{
-    name: "UI-LIB",
-    url: "https://ui-lib.com"
-  }],
-  keywords: ["e-commerce", "e-commerce template", "next.js", "react"]
+  authors: [
+    {
+      name: "UI-LIB",
+      url: "https://ui-lib.com",
+    },
+  ],
+  keywords: ["e-commerce", "e-commerce template", "next.js", "react"],
 };
 export default function IndexPage() {
-  return <FashionTwoPageView />;
+  return (
+    <ShopLayout1>
+      <FashionTwoPageView />
+    </ShopLayout1>
+  );
 }
